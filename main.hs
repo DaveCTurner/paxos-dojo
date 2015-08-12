b29550ba5a19b266b6e08875de21d474e3a448fc
David Turner <dave.c.turner@gmail.com>
2015-08-12 12:18:04 +0000

    Full coverage

--- main.hs
+++ main.hs
@@ -25,7 +25,9 @@ learner newMessage@(Accepted proposalId acceptorId value) = do
   when (any isMatch previousMessages) $ tell [value]
 
   where isMatch (Accepted proposalId' acceptorId' _)
-          = proposalId == proposalId' && acceptorId /= acceptorId'
+          | proposalId /= proposalId' = False
+          | acceptorId == acceptorId' = False
+          | otherwise                 = True
 
 data PromiseType = Free | Bound ProposalId Value deriving (Show, Eq)
 data PromisedMessage = Promised ProposalId AcceptorId PromiseType deriving (Show, Eq)
@@ -33,19 +35,15 @@ data ProposedMessage = Proposed ProposalId Value deriving (Show, Eq)
 
 data ProposerState = ProposerState (S.Set ProposalId) [PromisedMessage]
 
-proposer :: (MonadState ProposerState m, MonadWriter [ProposedMessage] m, MonadReader Value m) => PromisedMessage -> m ()
+proposer :: (MonadState ProposerState m, MonadWriter [ProposedMessage] m, MonadReader Value m, Functor m) => PromisedMessage -> m ()
 proposer newMessage@(Promised proposalId acceptorId promiseType) = do
   ProposerState proposalsMade previousMessages <- get
   unless (S.member proposalId proposalsMade) $ do
     put $ ProposerState proposalsMade $ newMessage : previousMessages
-    _ <- runMaybeT $ forM_ previousMessages proposeValueIfMatch
-    return ()
+    void $ runMaybeT $ forM_ previousMessages proposeValueIfMatch
 
   where
-  proposeValueIfMatch (Promised proposalId' acceptorId' promiseType')
-    | proposalId == proposalId'
-    , acceptorId /= acceptorId'
-    = do
+  proposeValueIfMatch (Promised proposalId' acceptorId' promiseType') = when isMatch $ do
       value <- case (promiseType, promiseType') of
         (Bound pid1 val1, Bound pid2 val2)
           | pid1 < pid2   -> return val2
@@ -56,8 +54,10 @@ proposer newMessage@(Promised proposalId acceptorId promiseType) = do
       tell [Proposed proposalId value]
       modify $ \(ProposerState proposalsMade msgs) -> ProposerState (S.insert proposalId proposalsMade) msgs
       mzero
-
-    | otherwise = return ()
+    where isMatch
+            | proposalId /= proposalId' = False
+            | acceptorId == acceptorId' = False
+            | otherwise                 = True
 
 main :: IO ()
 main = hspec $ do
@@ -76,12 +76,19 @@ main = hspec $ do
     ,(Accepted 4 "emily" "bar", ["bar"])
     ]
   describe "Proposer" $ proposerTest
-    [(Promised 1 "rosie" Free, [])
-    ,(Promised 3 "rosie" Free, [])
-    ,(Promised 1 "emily" Free, [Proposed 1 "my value"])
-    ,(Promised 1 "susan" Free, [])
-    ,(Promised 3 "emily" (Bound 2 "another value"), [Proposed 3 "another value"])
+    [(Promised 1 "rosie" Free,                          [])
+    ,(Promised 3 "rosie" Free,                          [])
+    ,(Promised 3 "rosie" Free,                          [])
+    ,(Promised 1 "emily" Free,                          [Proposed 1 "my value"])
+    ,(Promised 1 "susan" Free,                          [])
+    ,(Promised 3 "emily" (Bound 2 "another value"),     [Proposed 3 "another value"])
     ,(Promised 3 "susan" (Bound 1 "yet another value"), [])
+    ,(Promised 4 "susan" (Bound 1 "yet another value"), [])
+    ,(Promised 4 "rosie" Free,                          [Proposed 4 "yet another value"])
+    ,(Promised 5 "susan" (Bound 1 "yet another value"), [])
+    ,(Promised 5 "emily" (Bound 2 "another value"),     [Proposed 5 "another value"])
+    ,(Promised 6 "emily" (Bound 2 "another value"),     [])
+    ,(Promised 6 "susan" (Bound 1 "yet another value"), [Proposed 6 "another value"])
     ]
 
 learnerTest :: [(AcceptedMessage, [String])] -> SpecWith ()
