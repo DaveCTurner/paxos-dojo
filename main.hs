1542b9eb3616ff04497f86809dbbf7101ef0b5cd
David Turner <dave.c.turner@gmail.com>
2015-08-13 09:48:57 +0000

    Some acceptor impl

--- main.hs
+++ main.hs
@@ -14,6 +14,9 @@ type ProposalId = Int
 type AcceptorId = String
 type Value = String
 
+data PrepareMessage = Prepare ProposalId
+  deriving (Show, Eq)
+
 data AcceptedMessage = Accepted ProposalId AcceptorId Value
   deriving (Show, Eq)
 
@@ -60,6 +63,30 @@ proposer newMessage@(Promised proposalId acceptorId promiseType) = do
             | acceptorId == acceptorId' = False
             | otherwise                 = True
 
+data AcceptorState = AcceptorState (Maybe ProposalId) PromiseType
+
+acceptor
+  :: (MonadState AcceptorState m, MonadWriter [Either PromisedMessage AcceptedMessage] m, MonadReader AcceptorId m, Functor m)
+  => Either PrepareMessage ProposedMessage -> m ()
+
+acceptor (Left (Prepare proposalId)) = do
+  AcceptorState maybeMaxPromised promiseType <- get
+  case promiseType of
+    Bound proposalId' _ | proposalId <= proposalId' -> return ()
+    _ -> do
+      myName <- ask
+      tell [Left $ Promised proposalId myName promiseType]
+      put $ AcceptorState (max maybeMaxPromised $ Just proposalId) promiseType
+
+acceptor (Right (Proposed proposalId value)) = do
+  AcceptorState maybeMaxPromised promiseType <- get
+  when (maybeMaxPromised <= Just proposalId) $ case promiseType of
+    Bound proposalId' _ | proposalId' <= proposalId -> error "TODO" -- return ()
+    _ -> do
+      myName <- ask
+      tell [Right $ Accepted proposalId myName value]
+      put $ AcceptorState maybeMaxPromised $ Bound proposalId value
+
 main :: IO ()
 main = hspec $ do
   describe "Learner" $ learnerTest
@@ -93,6 +120,17 @@ main = hspec $ do
     ,(Promised 6 "alice" (Bound 1 "alice's value"), [Proposed 6 "brian's value"],
                                 "should propose the value given in the first promise as it has the higher-numbered value")
     ]
+  describe "Acceptor" $ acceptorTest
+    [(Left $ Prepare 2, [Left $ Promised 2 "me" Free], "should emit a promise for the first message")
+    ,(Left $ Prepare 1, [Left $ Promised 1 "me" Free], "should emit a promise for a lower-numbered proposal too")
+    ,(Left $ Prepare 2, [Left $ Promised 2 "me" Free], "should emit a promise for a duplicate proposal")
+    ,(Right $ Proposed 1 "a value", [], "should not accept a proposal for a lower proposal number")
+    ,(Right $ Proposed 2 "another value", [Right $ Accepted 2 "me" "another value"], "should not accept a proposal for a lower proposal number")
+    ,(Left $ Prepare 1, [], "should not emit a promise now that a > proposal has been accepted")
+    ,(Left $ Prepare 2, [], "should not emit a promise now that a == proposal has been accepted")
+    ,(Left $ Prepare 3, [Left $ Promised 3 "me" (Bound 2 "another value")],
+        "should emit a promise for a > proposal")
+    ]
 
 learnerTest :: [(AcceptedMessage, [String], String)] -> SpecWith ()
 learnerTest testSeq = forM_ (inits testSeq) $ \ioPairs ->
@@ -108,5 +146,13 @@ proposerTest testSeq = forM_ (inits testSeq) $ \ioPairs ->
       expectedOutputs = concat $ map snd3 ioPairs
       worksAsExpected = last $ "should propose nothing from no messages" : map thd3 ioPairs
       (_, _, outputs) = runRWS (mapM_ proposer inputs) "my value" (ProposerState S.empty [])
+  in it worksAsExpected $ outputs `shouldBe` expectedOutputs
+
+acceptorTest :: [(Either PrepareMessage ProposedMessage, [Either PromisedMessage AcceptedMessage], String)] -> SpecWith ()
+acceptorTest testSeq = forM_ (inits testSeq) $ \ioPairs ->
+  let inputs          =          map fst3 ioPairs
+      expectedOutputs = concat $ map snd3 ioPairs
+      worksAsExpected = last $ "should emit nothing from no messages" : map thd3 ioPairs
+      (_, _, outputs) = runRWS (mapM_ acceptor inputs) "me" (AcceptorState Nothing Free)
   in it worksAsExpected $ outputs `shouldBe` expectedOutputs
 
