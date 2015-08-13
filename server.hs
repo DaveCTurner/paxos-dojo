1f982eb26a501eed3756759bd8d3f05d9eacf30c
David Turner <dave.c.turner@gmail.com>
2015-08-13 14:26:11 +0000

    Now supports runtime config

--- server.hs
+++ server.hs
@@ -1,5 +1,6 @@
-{-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards #-}
 
 import Control.Applicative
 import Control.Concurrent
@@ -10,9 +11,11 @@ import Control.Monad
 import Data.Aeson hiding (Value)
 import Data.Monoid
 import Data.Time
-import Network.HTTP.Types
+import Data.Time.ISO8601
+import Network.HTTP.Types hiding (Status)
 import Network.Wai
 import Network.Wai.Handler.Warp
+import System.Random
 import Text.Printf
 import qualified Data.Map as M
 import qualified Data.ByteString as B
@@ -97,48 +100,124 @@ instance FromJSON PaxosMessage where
                 <*> o .: "max-accepted-value")
         <|> (FreePromised <$> proposalId <*> acceptorId)
 
+data Config = Config
+  { cGetTimeout     :: Integer
+  , cQueueExpiry    :: Integer
+  , cProposerCount  :: Integer
+  , cDropPercentage :: Double
+  , cMinDelay       :: Int
+  , cMaxDelay       :: Int
+  }
+
+instance ToJSON Config where
+  toJSON Config{..} = object
+    [ "get-timeout-us"  .= cGetTimeout
+    , "queue-expiry-us" .= cQueueExpiry
+    , "proposer-count"  .= cProposerCount
+    , "drop-percentage" .= cDropPercentage
+    , "min-delay-us"    .= cMinDelay
+    , "max-delay-us"    .= cMaxDelay
+    ]
+
+instance FromJSON Config where
+  parseJSON = withObject "Config" $ \o -> Config
+    <$> o .: "get-timeout-us"
+    <*> o .: "queue-expiry-us"
+    <*> o .: "proposer-count"
+    <*> o .: "drop-percentage"
+    <*> o .: "min-delay-us"
+    <*> o .: "max-delay-us"
+
+data Status = Status Config [(B.ByteString, UTCTime, Bool)]
+
+instance ToJSON Status where
+  toJSON (Status config queues) = object
+    [ "config" .= config
+    , "queues" .= map (\(queueName, lastGetTime, isEmpty) -> object
+        [ "queue" .= T.decodeUtf8 queueName
+        , "last-get-time" .= formatISO8601Micros lastGetTime
+        , "is-empty" .= isEmpty
+        ]) queues
+    ]
+
 main :: IO ()
 main = do
   outgoingQueueByNameVar <- newTVarIO M.empty
   incomingQueue          <- newTQueueIO
   logLock                <- newMVar ()
+  configVar              <- newTVarIO $ Config
+    { cGetTimeout     = 10000000
+    , cQueueExpiry    = 60000000
+    , cProposerCount  = 10
+    , cDropPercentage = 0
+    , cMinDelay       = 0
+    , cMaxDelay       = 0
+    }
+
   let logMessage :: UTCTime -> B.ByteString -> String -> IO ()
       logMessage time queueName message = void $ forkIO $ withMVar logLock $ \_ -> putStrLn
-        $ printf "%-23s %-15s %s" (take 23 $ show time) (T.unpack $ T.decodeUtf8 queueName) message
+        $ printf "%-27s %-15s %s" (formatISO8601Micros time) (T.unpack $ T.decodeUtf8 queueName) message
 
   withAsync (run 24192 $ \req respond -> do
     now <- getCurrentTime
     let queueName = rawPathInfo req
     case parseMethod $ requestMethod req of
-      Right GET -> do
+      Right GET
+        | queueName == "/status" -> do
 
-        theQueue <- atomically $ do
-          outgoingQueueByName <- readTVar outgoingQueueByNameVar
-          theQueue <- case M.lookup queueName outgoingQueueByName of
-            Nothing                 -> newTQueue
-            Just (_, existingQueue) -> return existingQueue
-          writeTVar outgoingQueueByNameVar $ M.insert queueName (now, theQueue) outgoingQueueByName
-          return theQueue
+            status <- atomically $ do
+              outgoingQueueByName <- readTVar outgoingQueueByNameVar
+              Status <$> readTVar configVar
+                     <*> forM (M.toList outgoingQueueByName)
+                  (\(queueName, (lastAccessTime, queue)) -> 
+                    (,,) queueName lastAccessTime <$> isEmptyTQueue queue)
 
-        response <- timeout 10000000 $ atomically $ readTQueue theQueue
+            respond $ responseLBS ok200 [(hContentType, T.encodeUtf8 "application/json")] $ encode status
 
-        case response of
-          Nothing -> respond $ responseBuilder noContent204 [] mempty
-          Just value -> do
-            let valueBytes = encode (value :: PaxosMessage)
-            responseTime <- getCurrentTime
-            logMessage responseTime queueName $ "GET  " ++ (T.unpack $ T.decodeUtf8 $ BL.toStrict valueBytes)
-            respond $ responseLBS ok200 [(hContentType, T.encodeUtf8 "application/json")] valueBytes
+        | queueName == "/config" -> do
 
-      Right POST -> do
-        body <- strictRequestBody req
-        let maybeValue = decode body
-        case maybeValue of
-          Nothing -> respond $ responseLBS badRequest400 [] mempty
-          Just value -> do
-            logMessage now queueName $ "POST " ++ (T.unpack $ T.decodeUtf8 $ BL.toStrict $ encode value)
-            atomically $ writeTQueue incomingQueue (queueName, now, value :: PaxosMessage)
-            respond $ responseBuilder noContent204 [] mempty
+            config <- atomically $ readTVar configVar
+            respond $ responseLBS ok200 [(hContentType, T.encodeUtf8 "application/json")] $ encode config
+
+        | otherwise -> do
+
+            (theQueue, Config{..}) <- atomically $ do
+              outgoingQueueByName <- readTVar outgoingQueueByNameVar
+              theQueue <- case M.lookup queueName outgoingQueueByName of
+                Nothing                 -> newTQueue
+                Just (_, existingQueue) -> return existingQueue
+              writeTVar outgoingQueueByNameVar $ M.insert queueName (now, theQueue) outgoingQueueByName
+              (,) theQueue <$> readTVar configVar
+
+            response <- timeout cGetTimeout $ atomically $ readTQueue theQueue
+
+            case response of
+              Nothing -> respond $ responseBuilder noContent204 [] mempty
+              Just value -> do
+                let valueBytes = encode (value :: PaxosMessage)
+                responseTime <- getCurrentTime
+                logMessage responseTime queueName $ "GET  " ++ (T.unpack $ T.decodeUtf8 $ BL.toStrict valueBytes)
+                respond $ responseLBS ok200 [(hContentType, T.encodeUtf8 "application/json")] valueBytes
+
+      Right POST
+        | queueName == "/config" -> do
+            body <- strictRequestBody req
+            let maybeValue = decode body
+            case maybeValue of
+              Nothing -> respond $ responseLBS badRequest400 [] mempty
+              Just value -> do
+                atomically $ writeTVar configVar value
+                respond $ responseBuilder noContent204 [] mempty
+
+        | otherwise -> do
+            body <- strictRequestBody req
+            let maybeValue = decode body
+            case maybeValue of
+              Nothing -> respond $ responseLBS badRequest400 [] mempty
+              Just value -> do
+                logMessage now queueName $ "POST " ++ (T.unpack $ T.decodeUtf8 $ BL.toStrict $ encode value)
+                atomically $ writeTQueue incomingQueue (queueName, now, value :: PaxosMessage)
+                respond $ responseBuilder noContent204 [] mempty
 
       _ -> respond $ responseLBS badRequest400 [] mempty)
 
@@ -147,13 +226,15 @@ main = do
     $ \_ -> forever $ join $ atomically $ do
 
         (queueName, receivedTime, value) <- readTQueue incomingQueue
+        Config{..} <- readTVar configVar
 
-        let staleIfNotQueriedSince = addUTCTime (-30) receivedTime
+        let staleIfNotQueriedSince = addUTCTime (fromIntegral cQueueExpiry * negate 0.000001) receivedTime
         (activeQueuesMap, staleQueuesMap) <- M.partition ((> staleIfNotQueriedSince) . fst)
             <$> readTVar outgoingQueueByNameVar
         writeTVar outgoingQueueByNameVar activeQueuesMap
 
-        let isRelevantProposer proposalId = (== (T.encodeUtf8 ("/proposer/" <> T.pack (show (mod proposalId 10)))))
+        let isRelevantProposer proposalId = (== (T.encodeUtf8 ("/proposer/" <> T.pack (show (mod proposalId cProposerCount)))))
+
             shouldOutputTo = case value of
               Prepare  {} -> B.isPrefixOf (T.encodeUtf8 "/acceptor/")
               Proposed {} -> B.isPrefixOf (T.encodeUtf8 "/acceptor/")
@@ -161,9 +242,17 @@ main = do
               FreePromised  proposalId _     -> isRelevantProposer proposalId
               BoundPromised proposalId _ _ _ -> isRelevantProposer proposalId
 
-        forM_ (M.toList activeQueuesMap) $ \(queueName, (_, queue))
-          -> when (shouldOutputTo queueName) $ writeTQueue queue value
+            outputQueues = [ queue
+                           | (queueName, (_, queue)) <- M.toList activeQueuesMap
+                           , shouldOutputTo queueName ]
 
         return $ do
           forM_ (M.keys staleQueuesMap) $ \staleQueueName ->
-            logMessage receivedTime staleQueueName "expired"
+            logMessage receivedTime staleQueueName ("expired at " <> formatISO8601Millis staleIfNotQueriedSince)
+          forM_ outputQueues $ \queue -> do
+            dropRV  <- randomRIO (0.0, 100.0)
+            delayRV <- randomRIO (cMinDelay, cMaxDelay)
+            when (dropRV >= cDropPercentage) $ void $ forkIO $ do
+              threadDelay delayRV
+              atomically $ writeTQueue queue value
+
