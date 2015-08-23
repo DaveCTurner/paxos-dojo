86a8f7f1f127bf5251f9c324f69f69121a10005d
David Turner <dave.c.turner@gmail.com>
2015-08-23 23:31:48 +0000

    Separate out python Messenger class

---
+++ src/python/messenger.py
@@ -0,0 +1,33 @@
+import urllib2
+import json
+import time
+
+class Messenger:
+  """Class for communicating with Paxos cluster"""
+  def __init__(self, url):
+    self.url = url
+
+  def getNextMessage(self):
+    while True:
+      try:
+        response = urllib2.urlopen(self.url)
+        try:
+          if response.code == 200:
+            return json.load(response)
+        finally:
+          response.close()
+      except ValueError as e:
+        print e
+        time.sleep(2.0)
+      except urllib2.URLError as e:
+        print e
+        time.sleep(2.0)
+
+  def postMessage(self, message):
+    req = urllib2.Request(self.url)
+    req.add_header('Content-Type', 'application/json')
+    try:
+      urllib2.urlopen(req, json.dumps(message))
+    except urllib2.URLError as e:
+      print e
+
--- src/python/skeleton.py
+++ src/python/skeleton.py
@@ -1,43 +1,19 @@
 #!/usr/bin/python
 
-import urllib2
-import json
-import time
+from messenger import Messenger
 
-url = 'http://127.0.0.1:24192/learner/5'
+messenger = Messenger('http://127.0.0.1:24192/learner/5');
 
-def getNextMessage():
-  while True:
-    try:
-      response = urllib2.urlopen(url)
-      try:
-        if response.code == 200:
-          return json.load(response)
-      finally:
-        response.close()
-    except ValueError as e:
-      print e
-      time.sleep(2.0)
-    except urllib2.URLError as e:
-      print e
-      time.sleep(2.0)
-
-def postMessage(message):
-  req = urllib2.Request(url)
-  req.add_header('Content-Type', 'application/json')
-  try:
-    urllib2.urlopen(req, json.dumps(message))
-  except urllib2.URLError as e:
-    print e
+proposal = 0;
 
 while True:
-  currMessage = getNextMessage()
+  currMessage = messenger.getNextMessage()
   print "Received: " + str(currMessage)
+
+  print "value: " + currMessage['value']
 
   ''' process currMessage as described, possibly sending back some other messages, e.g.:
 
-  print "Proposing", proposal
-  postMessage({'type':'prepare','proposal':proposal})
-  proposal += 1
+  messenger.postMessage({'type':'messageType','timePeriod':timePeriod})
   '''
 
