7045e7463ac45785685c1d303e048e2f4c2b492c
David Turner <dave.c.turner@gmail.com>
2015-09-10 07:40:54 +0000

    Split out ruby messenger

--- src/ruby/main.rb
+++ src/ruby/main.rb
@@ -1,44 +1,6 @@
 #!/usr/bin/ruby
 
-require 'net/http'
-require 'json'
-
-class Messenger
-  def initialize(url)
-    @url = URI(url)
-  end
-
-  def getMessage
-    while true do
-
-      begin
-        response = Net::HTTP.get_response(@url)
-        if response.code == '200' then
-          return JSON.parse(response.body)
-        end
-      rescue JSON::ParserError
-        sleep 2
-      rescue Errno::ECONNREFUSED
-        sleep 2
-      rescue EOFError
-        sleep 2
-      end
-
-    end
-  end
-
-  def sendMessage(message)
-    print "Sending '" + message.to_s + "\n"
-    begin
-      http = Net::HTTP.new(@url.host, @url.port)
-      req = Net::HTTP::Post.new(@url.path, initheader = {'Content-type' => 'application/json'})
-      req.body = message.to_json
-      http.request(req)
-    rescue Errno::ECONNREFUSED
-    rescue EOFError
-    end
-  end
-end
+require_relative 'messenger'
 
 class Learner
   def initialize
